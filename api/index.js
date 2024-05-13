f24ae348f3315d8774be0f5816cbeee02bdc6e88
Brandon Munongi <brandon.munongi@gmail.com>
2024-05-13 16:59:20 +0200

    Connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect("mongodb+srv://brandonmunongi:cIiYk3gOqiXxV7eD@mern.szwpi9k.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern")
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 const app = express();
 
